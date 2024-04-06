946b21f4b7890e42f103faa585d488436cf16ca7
AbdelhamedRagheb <x2912003x@gmail.com>
2024-04-06 15:30:03 +0200

    pyrhon script

---
+++ INTRODUCTION TO CRYPTOHACK/ASCII.py
@@ -0,0 +1,11 @@
+
+def decodeAscII(flag:list)->str:
+    #this fun convert ascii to the real massage 
+    return(''.join(chr(o) for o in flag)) #decode the massage 
+
+def encodeAscii(flag:str)->list:
+    #this fun convert massage to ascii 
+    return([ord(o) for o in flag]) # encode the massage in ASCII format 
+
+print(decodeAscII([99, 114, 121, 112, 116, 111, 123, 65, 83, 67, 73, 73, 95, 112, 114, 49, 110, 116, 52, 98, 108, 51, 125]))
+print(encodeAscii("crypto{ASCII_pr1nt4bl3}"))
